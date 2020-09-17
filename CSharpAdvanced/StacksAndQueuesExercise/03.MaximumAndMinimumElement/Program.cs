d74329759619da1ed678a49abb5e7b5dcb2669d8
Stefan Denchev <zirael@abv.bg>
2020-09-17 17:56:52 +0300

    More Ex

---
+++ CSharpAdvanced/StacksAndQueuesExercise/03.MaximumAndMinimumElement/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.MaximumAndMinimumElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                var elements = input.Split();
+                if (elements[0].Contains("1"))
+                {
+                    int num = int.Parse(elements[1]);
+                    stack.Push(num);
+                }
+                if (elements[0].Contains("2"))
+                {
+                    if (stack.Count > 0)
+                    {
+                        stack.Pop();
+                    }
+                }
+                if (elements[0].Contains("3"))
+                {
+                    if (stack.Count > 0)
+                    {
+                        Console.WriteLine(stack.Max());
+                    }
+                }
+                if (elements[0].Contains("4"))
+                {
+                    if (stack.Count > 0)
+                    {
+                        Console.WriteLine(stack.Min());
+                    }
+                }
+            }
+            Console.WriteLine(String.Join(", ", stack));
+        }
+    }
+}
---
+++ CSharpAdvanced/StacksAndQueuesExercise/08.BalancedParenthesis/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08.BalancedParenthesis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] input = Console.ReadLine().ToCharArray();
+
+            
+            Queue<char> queue = new Queue<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i < input.Length / 2)
+                {
+                    stack.Push(input[i]);
+                }
+                else
+                {
+                    queue.Enqueue(input[i]);
+                }
+            }
+            bool isBalanced = true;
+            while (stack.Count > 0)
+            {
+                char first = stack.Pop();
+                char second = queue.Dequeue();
+
+                if (!(first == '[' && second == ']'
+                     || first == '(' && second == ')'
+                     || first == '{' && second == '}'))
+                {
+                    isBalanced = false;
+                    break;
+                }
+            }
+
+            if (isBalanced && queue.Count == 0)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
