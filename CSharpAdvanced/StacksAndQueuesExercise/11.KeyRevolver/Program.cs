e5a60c6bf8f4067025fba3f6a5acf827da42adbd
Stefan Denchev <zirael@abv.bg>
2020-09-24 11:29:30 +0300

    Stacks and queue exercise

---
+++ CSharpAdvanced/StacksAndQueuesExercise/11.KeyRevolver/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _11.KeyRevolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bulletPrice = int.Parse(Console.ReadLine());
+            int barrelSize = int.Parse(Console.ReadLine());
+
+            var stackNums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> bullets = new Stack<int>(stackNums);
+
+            var queueNums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> locks = new Queue<int>(queueNums);
+
+            int value = int.Parse(Console.ReadLine());
+            int bulletsUsed = 0;
+            int barrelCount = 0;
+
+            while (bullets.Any() && locks.Any())
+            {
+                if (bullets.Pop() <= locks.Peek())
+                {
+                    Console.WriteLine("Bang!");
+                    locks.Dequeue();
+                }
+                else
+                {
+                    Console.WriteLine("Ping!");
+                }
+                barrelCount++;
+                if (barrelSize == barrelCount && bullets.Any())
+                {
+                    Console.WriteLine("Reloading!");
+                    barrelCount = 0;
+                }
+                bulletsUsed++;
+            }
+
+            if (bullets.Count == 0 && locks.Count > 0)
+            {
+                Console.WriteLine($"Couldn't get through. Locks left: {locks.Count}");
+            }
+            else
+            {
+                int bulletPriceTotal = bulletPrice * bulletsUsed;
+                int potentialMoney = value - bulletPriceTotal;
+                Console.WriteLine($"{bullets.Count} bullets left. Earned ${potentialMoney}");
+            }
+        }
+    }
+}
---
+++ CSharpAdvanced/StacksAndQueuesExercise/12.CupsAndBottles/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _12.CupsAndBottles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queueNums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> cups = new Queue<int>(queueNums);
+
+            var stackNums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> bottles = new Stack<int>(stackNums);
+
+            int wastedWater = 0;
+
+            while (cups.Any() && bottles.Any())
+            {
+                int currentCup = cups.Peek(); 
+                int currentBottle = bottles.Peek();
+
+                if (currentBottle >= currentCup)
+                {
+                    wastedWater += currentBottle - currentCup;
+                    cups.Dequeue();
+                    bottles.Pop();
+                }
+                else
+                {
+                    currentCup -= currentBottle;
+                    bottles.Pop();
+                    while (currentCup > 0)
+                    {
+                        currentBottle = bottles.Peek();
+                        if (currentBottle >= currentCup)
+                        {
+                            wastedWater += currentBottle - currentCup;
+                            currentCup = 0;
+                            cups.Dequeue();
+                            bottles.Pop();
+                        }
+                        else
+                        {
+                            currentCup -= currentBottle;
+                            bottles.Pop();
+                        }
+                    }
+
+                }
+            }
+            if (bottles.Count == 0 && cups.Count > 0)
+            {
+                Console.WriteLine($"Cups: {String.Join(" ", cups)}");
+            }
+            else
+            {
+                Console.WriteLine($"Bottles: {String.Join(" ", bottles)}");
+            }
+            Console.WriteLine($"Wasted litters of water: {wastedWater}");
+        }
+    }
+}
