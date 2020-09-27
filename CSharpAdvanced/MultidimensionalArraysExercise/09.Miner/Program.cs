b3da4ee31096433f836364f166a652603657e2d8
Stefan Denchev <zirael@abv.bg>
2020-09-27 16:41:50 +0300

    *tasks

--- CSharpAdvanced/MultidimensionalArraysExercise/09.Miner/Program.cs
+++ CSharpAdvanced/MultidimensionalArraysExercise/09.Miner/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace _09.Miner
@@ -8,21 +9,170 @@ namespace _09.Miner
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
             var matrix = new char[n, n];
             FillMatrix(matrix);
 
-            string directions = Console.ReadLine();
             int coalFound = 0;
+            int coalCount = 0;
+            coalCount = CoalCount(n, matrix, coalCount);
 
-            
+            int minerRow = 0;
+            int minerCol = 0;
 
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    if (matrix[row, col] == 's')
+                    {
+                        minerRow = row;
+                        minerCol = col;
+                    }
+                    
+                }
+            }
+
+            var directionsArr = input.Split();
+            Queue<string> directions = new Queue<string>(directionsArr);
+
+            bool end = false;
+
+            while (true)
+            {
+                string currDirection = directions.Dequeue();
+                if (directions.Count == 0)
+                {
+                    end = true;
+                }
+
+                if (currDirection == "up")
+                {
+                    if (!(minerRow - 1 < 0))
+                    {
+                        minerRow--;
+                        if (matrix[minerRow,minerCol] == 'c')
+                        {
+                            coalFound++;
+                            matrix[minerRow, minerCol] = '*';
+                            if (coalCount == coalFound)
+                            {
+                                Console.WriteLine($"You collected all coals! ({minerRow}, {minerCol})");
+                                return;
+                            }
+                        }
+
+                        else if (matrix[minerRow, minerCol] == 'e')
+                        {
+                            Console.WriteLine($"Game over! ({minerRow}, {minerCol})");
+                            return;
+                        }
+
+                    }
+                }
+
+
+                if (currDirection == "down")
+                {
+                    if (!(minerRow + 1 > n - 1))
+                    {
+                        minerRow++;
+                        if (matrix[minerRow, minerCol] == 'c')
+                        {
+                            coalFound++;
+                            matrix[minerRow, minerCol] = '*';
+                            if (coalCount == coalFound)
+                            {
+                                Console.WriteLine($"You collected all coals! ({minerRow}, {minerCol})");
+                                return;
+                            }
+                        }
+                        else if (matrix[minerRow, minerCol] == 'e')
+                        {
+                            Console.WriteLine($"Game over! ({minerRow}, {minerCol})");
+                            return;
+                        }
+                    }
+                }
+
+                if (currDirection == "right")
+                {
+                    if (!(minerCol + 1 > n - 1))
+                    {
+                        minerCol++;
+                        if (matrix[minerRow, minerCol] == 'c')
+                        {
+                            coalFound++;
+                            matrix[minerRow, minerCol] = '*';
+                            if (coalCount == coalFound)
+                            {
+                                Console.WriteLine($"You collected all coals! ({minerRow}, {minerCol})");
+                                return;
+                            }
+                        }
+                        else if (matrix[minerRow, minerCol] == 'e')
+                        {
+                            Console.WriteLine($"Game over! ({minerRow}, {minerCol})");
+                            return;
+                        }
+                    }
+                }
+
+                if (currDirection == "left")
+                {
+                    if (!(minerCol - 1 < 0))
+                    {
+                        minerCol--;
+                        if (matrix[minerRow, minerCol] == 'c')
+                        {
+                            coalFound++;
+                            matrix[minerRow, minerCol] = '*';
+                            if (coalCount == coalFound)
+                            {
+                                Console.WriteLine($"You collected all coals! ({minerRow}, {minerCol})");
+                                return;
+                            }
+                        }
+                        else if (matrix[minerRow, minerCol] == 'e')
+                        {
+                            Console.WriteLine($"Game over! ({minerRow}, {minerCol})");
+                            return;
+                        }
+                    }
+                }
+
+                if (end)
+                {
+                    int coalsLeft = coalCount - coalFound;
+                    Console.WriteLine($"{coalsLeft} coals left. ({minerRow}, {minerCol})");
+                    return;
+                }
+
+            }
+
+        }
+
+        private static int CoalCount(int n, char[,] matrix, int coalCount)
+        {
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    if (matrix[col, row] == 'c')
+                    {
+                        coalCount++;
+                    }
+                }
+            }
+
+            return coalCount;
         }
 
         private static void FillMatrix(char[,] numbers)
         {
             for (int row = 0; row < numbers.GetLength(0); row++)
             {
-                char[] currentRow = Console.ReadLine().Split().Select(char.Parse).ToArray();
+                char[] currentRow = Console.ReadLine().Split(' ').Select(char.Parse).ToArray();
                 for (int col = 0; col < numbers.GetLength(1); col++)
                 {
                     numbers[row, col] = currentRow[col];
---
+++ CSharpAdvanced/MultidimensionalArraysExercise/10.RadioactiveMutantVampireBunnies/Program.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _10.RadioactiveMutantVampireBunnies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int rows = input[0];
+            int cols = input[1];
+            List<int[]> bunnies = new List<int[]>();
+            var matrix = new char[rows, cols];
+            FillMatrix(matrix);
+
+            int playerRow = 0;
+            int playerCol = 0;
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    if (matrix[row, col] == 'P')
+                    {
+                        playerRow = row;
+                        playerCol = col;
+                    }
+                }
+            }
+
+            var directionsArr = Console.ReadLine().ToCharArray();
+            Queue<char> directions = new Queue<char>(directionsArr);
+            bool dead = false;
+            bool won = false;
+
+            while (true)
+            {
+                char direction = directions.Dequeue();
+
+                if (direction == 'U')
+                {
+                    if (!(playerRow - 1 < 0))
+                    {
+                        matrix[playerRow, playerCol] = '.';
+                        playerRow--;
+                        matrix[playerRow, playerCol] = 'P';
+
+                        if (matrix[playerRow, playerCol] == 'B')
+                        {
+                            PrintMatrix(matrix);
+                            Console.WriteLine($"dead: {playerRow} {playerCol}");
+                            return;
+                        }
+
+                    }
+
+                    else
+                    {
+                        matrix[playerRow, playerCol] = '.';
+                        won = true;
+                    }
+
+                }
+
+
+                if (direction == 'D')
+                {
+                    if (!(playerRow + 1 > rows - 1))
+                    {
+                        matrix[playerRow, playerCol] = '.';
+                        playerRow++;
+                        matrix[playerRow, playerCol] = 'P';
+
+                        if (matrix[playerRow, playerCol] == 'B')
+                        {
+                            PrintMatrix(matrix);
+                            Console.WriteLine($"dead: {playerRow} {playerCol}");
+                            return;
+                        }
+
+                    }
+
+                    else
+                    {
+                        matrix[playerRow, playerCol] = '.';
+                        won = true;
+                    }
+
+                }
+
+                if (direction == 'R')
+                {
+                    if (!(playerCol + 1 > cols - 1))
+                    {
+                        matrix[playerRow, playerCol] = '.';
+                        playerCol++;
+                        matrix[playerRow, playerCol] = 'P';
+
+                        if (matrix[playerRow, playerCol] == 'B')
+                        {
+                            PrintMatrix(matrix);
+                            Console.WriteLine($"dead: {playerRow} {playerCol}");
+                            return;
+                        }
+
+                    }
+
+                    else
+                    {
+                        matrix[playerRow, playerCol] = '.';
+                        won = true;
+                    }
+
+                }
+
+
+                if (direction == 'L')
+                {
+                    if (!(playerCol - 1 < 0))
+                    {
+                        matrix[playerRow, playerCol] = '.';
+                        playerCol--;
+                        matrix[playerRow, playerCol] = 'P';
+
+                        if (matrix[playerRow, playerCol] == 'B')
+                        {
+                            PrintMatrix(matrix);
+                            Console.WriteLine($"dead: {playerRow} {playerCol}");
+                            return;
+                        }
+
+                    }
+
+                    else
+                    {
+                        matrix[playerRow, playerCol] = '.';
+                        won = true;
+                    }
+
+                }
+
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    for (int col = 0; col < matrix.GetLength(1); col++)
+                    {
+                        if (matrix[row, col] == 'B')
+                        {
+                            int[] currentBunny = { row, col };
+                            if (!bunnies.Contains(currentBunny))
+                            {
+                                bunnies.Add(currentBunny);
+                            }
+                        }
+                    }
+                }
+
+                for (int i = 0; i < bunnies.Count; i++)
+                {
+                    int[] currentBunny = bunnies[i];
+                    int row = currentBunny[0];
+                    int col = currentBunny[1];
+
+
+                    if (matrix[row, col] == 'B')
+                    {
+                        if (!(row - 1 < 0))
+                        {
+                            if (matrix[row - 1, col] == 'P')
+                            {
+                                dead = true;
+                            }
+                            matrix[row - 1, col] = 'B';
+                        }
+
+                        if (!(row + 1 > rows - 1))
+                        {
+                            if (matrix[row + 1, col] == 'P')
+                            {
+                                dead = true;
+                            }
+                            matrix[row + 1, col] = 'B';
+                        }
+
+                        if (!(col - 1 < 0))
+                        {
+                            if (matrix[row, col - 1] == 'P')
+                            {
+                                dead = true;
+                            }
+                            matrix[row, col - 1] = 'B';
+                        }
+
+                        if (!(col + 1 > cols - 1))
+                        {
+                            if (matrix[row, col + 1] == 'P')
+                            {
+                                dead = true;
+                            }
+                            matrix[row, col + 1] = 'B';
+                        }
+
+                    }
+                    else if (matrix[row, col] == 'P')
+                    {
+                        matrix[row, col] = 'B';
+                        dead = true;
+                    }
+                }
+
+                if (dead)
+                {
+                    PrintMatrix(matrix);
+                    Console.WriteLine($"dead: {playerRow} {playerCol}");
+                    return;
+                }
+                if (won)
+                {
+                    PrintMatrix(matrix);
+                    Console.WriteLine($"won: {playerRow} {playerCol}");
+                    return;
+                }
+            }
+
+        }
+
+        private static void FillMatrix(char[,] numbers)
+        {
+            for (int row = 0; row < numbers.GetLength(0); row++)
+            {
+                char[] currentRow = Console.ReadLine().ToCharArray();
+                for (int col = 0; col < numbers.GetLength(1); col++)
+                {
+                    numbers[row, col] = currentRow[col];
+                }
+            }
+        }
+
+        private static void PrintMatrix(char[,] numbers)
+        {
+            for (int row = 0; row < numbers.GetLength(0); row++)
+            {
+                for (int col = 0; col < numbers.GetLength(1); col++)
+                {
+                    Console.Write(numbers[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
