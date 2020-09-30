01657f5b1dba8192fb34aa9f14e644f242fd1053
Stefan Denchev <zirael@abv.bg>
2020-09-30 22:09:45 +0300

    StreamsFilesDirectoriesLab

---
+++ CSharpAdvanced/StreamsFilesDirectoriesLab/01.OddLines/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace _01.OddLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("../../../Program.cs"))
+            {
+                string line = reader.ReadLine();
+                int index = 0;
+                while (line != null)
+                {
+                    if (index % 2 == 1)
+                    {
+                        Console.WriteLine(line);
+                    }
+
+                    index++;
+                    line = reader.ReadLine();
+                }
+            }
+        }
+    }
+}
