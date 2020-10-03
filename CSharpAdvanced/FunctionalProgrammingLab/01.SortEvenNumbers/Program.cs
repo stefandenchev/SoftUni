78f31ffda913223197d1a1cae9d31d11a87430fd
Stefan Denchev <zirael@abv.bg>
2020-10-03 11:10:19 +0300

    More Ex

---
+++ CSharpAdvanced/FunctionalProgrammingLab/01.SortEvenNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.SortEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split(", ").Select((number) =>
+            {
+                return int.Parse(number);
+            })
+                .Where(x => x % 2 == 0)
+                .OrderBy((int x) =>
+                {
+                    return x;
+                }).ToArray();
+
+            Console.WriteLine(String.Join(", ", numbers));
+        }
+    }
+}
--- CSharpAdvanced/StreamsFilesDirectoriesExercise/03.WordCount/Program.cs
+++ CSharpAdvanced/StreamsFilesDirectoriesExercise/03.WordCount/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace _03.WordCount
 {
@@ -6,7 +11,47 @@ namespace _03.WordCount
     {
         static void Main(string[] args)
         {
-             
+            var dictionary = new Dictionary<string, int>();
+            string inputWords = File.ReadAllText("../../../words.txt");
+            string[] words = inputWords.Split();
+            words = words.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+            using var writer = new StreamWriter("../../../actualResult.txt");
+            using var writer2 = new StreamWriter("../../../expectedResult.txt");
+
+            using (var reader = new StreamReader("../../../text.txt"))
+            {
+                string currentSentence = reader.ReadLine();
+
+                while (currentSentence != null)
+                {
+                    foreach (var word in words)
+                    {
+                        if (currentSentence.ToLower().Contains(word))
+                        {
+
+                            if (!dictionary.ContainsKey(word))
+                            {
+                                dictionary.Add(word, 0);
+                                dictionary[word]++;
+                            }
+                            else
+                            {
+                                dictionary[word]++;
+                            }
+                        }
+                    }
+
+                    currentSentence = reader.ReadLine();
+                }
+                foreach (var word in dictionary)
+                {
+                    writer.WriteLine($"{word.Key} - {word.Value}");
+                }
+                foreach (var word in dictionary.OrderByDescending(x => x.Value))
+                {
+                    writer2.WriteLine($"{word.Key} - {word.Value}");
+                }
+            }
         }
     }
 }
