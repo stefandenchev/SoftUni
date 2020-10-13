85049538ae88440ba5ac301e2f7e5eb94a3b7fa1
Stefan Denchev <zirael@abv.bg>
2020-10-13 22:18:51 +0300

    GenericsLab

---
+++ CSharpAdvanced/GenericsLab/GenericArrayCreator/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenericArrayCreator
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine(String.Join(",", ArrayCreator.Create(10, "pesho")));
+        }
+    }
+}
