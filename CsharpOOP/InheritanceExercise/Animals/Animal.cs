e79af205782024b64105e51c5d616a72e14c3d07
Stefan Denchev <zirael@abv.bg>
2020-10-31 11:04:14 +0200

    Inheritance and Encapsulation

---
+++ CsharpOOP/InheritanceExercise/Animals/Animal.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public abstract class Animal
+    {
+
+        private string name;
+        private int age;
+        private string gender;
+
+        public Animal(string name, int age, string gender)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+        }
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+
+                this.name = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+
+                this.age = value;
+            }
+        }
+
+        public string Gender
+        {
+            get
+            {
+                return this.gender;
+            }
+
+            set
+            {
+                if (value != "Male" && value != "Female")
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+
+                this.gender = value;
+            }
+        }
+
+        public abstract string ProduceSound();
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(this.GetType().Name);
+            sb.AppendLine($"{this.name} {this.age} {this.gender}");
+            sb.AppendLine(ProduceSound());
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ CsharpOOP/InheritanceExercise/Animals/StartUp.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> animals = new List<Animal>();
+
+            string line1;
+
+            while ((line1 = Console.ReadLine()) != "Beast!")
+            {
+                var elements = Console.ReadLine().Split();
+                string name = elements[0];
+                int age = int.Parse(elements[1]);
+                string gender = null;
+
+                if (elements.Length >= 3)
+                {
+                    gender = elements[2];
+                }
+
+                Animal animal = null;
+
+                switch (line1)
+                {
+                    case "Dog":
+                        animal = new Dog(name, age, gender);
+                        break;
+
+                    case "Frog":
+                        animal = new Frog(name, age, gender);
+                        break;
+
+                    case "Cat":
+                        animal = new Cat(name, age, gender);
+                        break;
+
+                    case "Kitten":
+                        animal = new Kitten(name, age, gender);
+                        break;
+
+                    case "Tomcat":
+                        animal = new Tomcat(name, age, gender);
+                        break;
+
+                    default:
+                        throw new ArgumentException("Invalid input!");
+                }
+
+                animals.Add(animal);
+            }
+
+            foreach (Animal animal in animals)
+            {
+                Console.WriteLine(animal);
+            }
+
+        }
+    }
+}
