fa1e11a3474da1c7283319136ab9fc926dd51ff9
Stefan Denchev <zirael@abv.bg>
2020-12-06 10:49:06 +0200

    Algo

---
+++ Algorithms Fundamentals/RecursionAndBackTrackingLab/01.RecursiveArraySum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.RecursiveArraySum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(GetSum(array, 0));
+        }
+
+        private static int GetSum(int[] array, int index)
+        {
+            if (index == array.Length - 1)
+            {
+                return array[index];
+            }
+
+            return array[index] + GetSum(array, index + 1);
+        }
+    }
+}
---
+++ Algorithms Fundamentals/RecursionAndBackTrackingLab/02.RecursiveDrawing/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _02.RecursiveDrawing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            DrawFigure(n);
+        }
+
+        private static void DrawFigure(int size)
+        {
+            if (size == 0)
+            {
+                return;
+            }
+            Console.WriteLine(new string('*', size));
+
+            DrawFigure(size - 1);
+
+            Console.WriteLine(new string('#', size));
+        }
+    } 
+}
