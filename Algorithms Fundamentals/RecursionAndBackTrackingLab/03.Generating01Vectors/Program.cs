d3d6ab95f5326aab0ffb6cc6121a913c77d28dfa
Stefan Denchev <zirael@abv.bg>
2020-12-06 16:21:52 +0200

    Algo

---
+++ Algorithms Fundamentals/RecursionAndBackTrackingLab/03.Generating01Vectors/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _03.Generating01Vectors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var vector = new int[n];
+
+            GenerateVector(vector, 0);
+        }
+
+        private static void GenerateVector(int[] vector, int index)
+        {
+            if (index == vector.Length)
+            {
+                Console.WriteLine(string.Join("", vector));
+                return;
+            }
+            for (int number = 0; number <= 1; number++)
+            {
+                vector[index] = number;
+                GenerateVector(vector, index + 1);
+            }
+        }
+    }
+}
