142d80c9e75ce2a459ed8294f3696a5a251701b0
Stefan Denchev <zirael@abv.bg>
2021-02-10 20:38:37 +0200

    Update TripService.sql

--- DB/Exams/21 Jun 2020/TripService.sql
+++ DB/Exams/21 Jun 2020/TripService.sql
@@ -120,16 +120,90 @@ SELECT TOP(10)
 	c.CountryCode,
 	COUNT(*) AS Accounts
 FROM Cities c
-JOIN Accounts a ON a.CityId =  c.Id
+JOIN Accounts a ON a.CityId = c.Id
 GROUP BY c.Id, c.Name, c.CountryCode
 ORDER BY Accounts DESC
 
 --09. Romantic Getaways
-SELECT *
-FROM Accounts a
+SELECT 
+	a.Id,
+	a.Email,
+	c.Name,
+	COUNT(*) AS Trips
+FROM AccountsTrips ac
+JOIN Accounts a ON ac.AccountId = a.Id
 JOIN Cities c ON a.CityId = c.Id
-JOIN Hotels h ON h.CityId = c.Id
-JOIN AccountsTrips ac ON ac.AccountId = a.Id
 JOIN Trips t ON t.Id = ac.TripId
+JOIN Rooms r ON r.Id = t.RoomId
+JOIN Hotels h ON r.HotelId = h.Id
 WHERE c.Id = h.CityId
-ORDER BY a.Id
+GROUP BY a.Id, a.Email, c.Name
+ORDER BY Trips DESC, a.Id
+
+--10. GDPR Violation
+SELECT 
+	t.Id,
+	a.FirstName + ' ' + + ISNULL(a.MiddleName + ' ', '') + a.LastName AS [Full Name],
+	c.Name AS [From],
+	hc.Name AS [To],
+	CASE
+		WHEN t.CancelDate IS NULL THEN CONVERT(NVARCHAR, DATEDIFF(DAY, t.ArrivalDate, t.ReturnDate)) + ' days'
+		ELSE 'Canceled'
+	END AS Duration
+FROM AccountsTrips ac
+JOIN Accounts a ON ac.AccountId = a.Id
+JOIN Cities c ON a.CityId = c.Id
+JOIN Trips t ON t.Id = ac.TripId
+JOIN Rooms r ON r.Id = t.RoomId
+JOIN Hotels h ON r.HotelId = h.Id
+JOIN Cities hc ON hc.Id = h.CityId
+ORDER BY [Full Name], t.Id
+
+--11. Available Room
+CREATE FUNCTION udf_GetAvailableRoom(@HotelId INT, @Date DATE, @People INT)
+RETURNS NVARCHAR(MAX)
+BEGIN
+	DECLARE @RoomInfo VARCHAR(MAX) = (SELECT TOP(1) 'Room ' + CONVERT(VARCHAR, r.Id) + ': ' + r.Type + ' (' + CONVERT(VARCHAR, r.Beds) + ' beds) - $' + CONVERT(VARCHAR, (BaseRate + Price) * @People)
+	FROM Rooms r
+	JOIN Hotels h ON h.Id = r.HotelId
+	WHERE Beds >= @People AND HotelId = @HotelId AND
+		NOT EXISTS (SELECT * FROM Trips t WHERE t.RoomId = r.Id
+											AND CancelDate IS NULL 
+											AND @Date BETWEEN ArrivalDate AND ReturnDate)
+	ORDER BY (BaseRate + Price) * @People DESC)
+
+	IF (@RoomInfo IS NULL) RETURN 'No rooms available'
+	RETURN @RoomInfo
+END
+
+SELECT dbo.udf_GetAvailableRoom(112, '2011-12-17', 2)
+SELECT dbo.udf_GetAvailableRoom(94, '2015-07-26', 3)
+
+--12. Switch Room
+CREATE PROCEDURE usp_SwitchRoom(@TripId INT, @TargetRoomId INT)
+AS
+	DECLARE @TripHotelId INT = (SELECT r.HotelId
+	FROM Trips t
+	JOIN Rooms r ON t.RoomId = r.Id
+	WHERE t.Id = @TripId)
+
+	DECLARE @TargetRoomHotelId INT = (SELECT HotelId FROM Rooms
+													 WHERE Id = @TargetRoomId)
+
+	IF (@TripHotelId != @TargetRoomHotelId)
+		THROW 50001, 'Target room is in another hotel!', 1
+
+	DECLARE @TripAccounts INT = (SELECT COUNT(*) FROM AccountsTrips WHERE TripId = @TripId);
+	DECLARE @TargetRoomBeds INT = (SELECT Beds FROM Rooms WHERE Id = @TargetRoomId)
+	
+	IF (@TripAccounts > @TargetRoomBeds)
+		THROW 50002, 'Not enough beds in target room!', 1
+
+	UPDATE Trips
+	SET RoomId = @TargetRoomId WHERE Id = @TripId
+	
+
+EXEC usp_SwitchRoom 10, 11
+SELECT RoomId FROM Trips WHERE Id = 10
+EXEC usp_SwitchRoom 10, 7
+EXEC usp_SwitchRoom 10, 8
