92b20f6970bed940df6b0cb64a90808e4ae1e0fe
Stefan Denchev <zirael@abv.bg>
2021-02-22 20:36:03 +0200

    MiniORM

---
+++ DB-EF Core/02. ORM Fundamentals/02. ORM-Fundamentals-MiniORM-Lab-Skeleton/MiniORM.App/Program.cs
@@ -0,0 +1,30 @@
+﻿using MiniORM.App.Data;
+using MiniORM.App.Data.Entities;
+using System;
+using System.Linq;
+
+namespace MiniORM.App
+{
+    class Program
+    {
+		public static void Main(string[] args)
+		{
+			var connectionString = "Server=.;Database=MiniORM;Integrated Security=true";
+
+			var context = new SoftUniDbContext(connectionString);
+
+			context.Employees.Add(new Employee
+			{
+				FirstName = "Kaladin",
+				LastName = "Stormblessed",
+				DepartmentId = context.Departments.First().Id,
+				IsEmployed = true,
+			});
+
+			var employee = context.Employees.Last();
+			employee.FirstName = "Modified";
+
+			context.SaveChanges();
+		}
+	}
+}
