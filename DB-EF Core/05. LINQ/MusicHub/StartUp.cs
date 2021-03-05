c38533868503dd4ad5f0a6856e176af6938aec44
Stefan Denchev <zirael@abv.bg>
2021-03-05 13:20:35 +0200

    LINQ

---
+++ DB-EF Core/05. LINQ/MusicHub/StartUp.cs
@@ -0,0 +1,118 @@
+﻿namespace MusicHub
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            MusicHubDbContext context = 
+                new MusicHubDbContext();
+
+            DbInitializer.ResetDatabase(context);
+
+            //int producerId = int.Parse(Console.ReadLine());
+            int songLength = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine(ExportAlbumsInfo(context, producerId));
+            Console.WriteLine(ExportSongsAboveDuration(context, songLength));
+
+        }
+
+        public static string ExportAlbumsInfo(MusicHubDbContext context, int producerId)
+        {
+            var albums = context.Producers
+                .FirstOrDefault(x => x.Id == producerId)
+                .Albums
+                .Select(x => new
+                {
+                    AlbumName = x.Name,
+                    ReleaseDate = x.ReleaseDate,
+                    AlbumPrice = x.Price,
+                    ProducerName = x.Producer.Name,
+                    AlbumSongs = x.Songs.Select(x => new
+                    {
+                        SongName = x.Name,
+                        SongPrice = x.Price,
+                        SongWriterName = x.Writer.Name
+                    }).OrderByDescending(x => x.SongName)
+                    .ThenBy(x => x.SongWriterName)
+                    .ToList()
+                }).OrderByDescending(x => x.AlbumPrice)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var album in albums)
+            {
+                sb
+                    .AppendLine($"-AlbumName: {album.AlbumName}")
+                    .AppendLine($"-ReleaseDate: {album.ReleaseDate:MM/dd/yyyy}")
+                    .AppendLine($"-ProducerName: {album.ProducerName}")
+                    .AppendLine($"-Songs:");
+
+                    int counter = 1;
+
+                foreach (var song in album.AlbumSongs)
+                {
+                    sb
+                    .AppendLine($"---#{counter++}")
+                    .AppendLine($"---SongName: {song.SongName}")
+                    .AppendLine($"---Price: {song.SongPrice:f2}")
+                    .AppendLine($"---Writer: {song.SongWriterName}");
+                }
+
+                sb.AppendLine($"-AlbumPrice: {album.AlbumPrice:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string ExportSongsAboveDuration(MusicHubDbContext context, int duration)
+        {
+            var songs = context.Songs
+                .ToList()
+                .Where(x => x.Duration.TotalSeconds > duration)
+                .Select(x => new
+                {
+                    Name = x.Name,
+
+                    PerformerFullName = x.SongPerformers.Select(x => 
+                    x.Performer.FirstName + " " + x.Performer.LastName).FirstOrDefault(),
+                    WriterName = x.Writer.Name,
+                    AlbumProducer = x.Album.Producer.Name,
+                    Duration = x.Duration
+                }).OrderBy(x => x.Name)
+                .ThenBy(x => x.WriterName)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+
+            int counter = 1;
+
+            foreach (var song in songs)
+            {
+                sb.AppendLine($"-Song #{counter++}")
+                  .AppendLine($"---SongName: {song.Name}")
+                  .AppendLine($"---Writer: {song.WriterName}")
+                  .AppendLine($"---Performer: {song.PerformerFullName}")
+                  .AppendLine($"---AlbumProducer: {song.AlbumProducer}")
+                  .AppendLine($"---Duration: {song.Duration:c}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
+
+
+//-Song #1
+//---SongName: Away
+//---Writer: Norina Renihan
+//---Performer: Lula Zuan
+//---AlbumProducer: Georgi Milkov
+//---Duration: 00:05:35
