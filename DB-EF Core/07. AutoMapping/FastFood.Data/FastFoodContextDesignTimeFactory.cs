e4b45beaa3f239179ac67e5099d1c40aea37e0ac
Stefan Denchev <zirael@abv.bg>
2021-03-10 19:43:01 +0200

    07. AutoMapping

---
+++ DB-EF Core/07. AutoMapping/FastFood.Data/FastFoodContextDesignTimeFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FastFood.Data
+{
+    public class FastFoodContextDesignTimeFactory : IDesignTimeDbContextFactory<FastFoodContext>
+    {
+        public FastFoodContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<FastFoodContext>();
+            builder.UseSqlServer("Server=DESKTOP-H1L2UK7\\MSSQLSERVER01;Database=FastFood;Trusted_Connection=True;MultipleActiveResultSets=true");
+
+            return new FastFoodContext(builder.Options);
+        }
+    }
+}
