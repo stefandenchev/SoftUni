a55334480ca56b4e160f07ae6b10879234766010
Stefan Denchev <65227182+stefandenchev@users.noreply.github.com>
2021-10-16 11:30:59 +0300

    Exam prep

--- JS Advanced/Exams/12-08-20/02-movie.js
+++ JS Advanced/Exams/12-08-20/02-movie.js
@@ -1,12 +1,59 @@
 class Movie {
     constructor(movieName, ticketPrice) {
         this.movieName = movieName;
-        this.ticketPrice = ticketPrice;
+        this.ticketPrice = Number(ticketPrice);
         this.screenings = [];
+        this.totalProfit = 0;
+        this.totalTicketsSold = 0;
     }
 
     newScreening(date, hall, description) {
-        
+        if (this.screenings.some(s => s.date == date && s.hall == hall)) {
+            throw new Error(`Sorry, ${hall} hall is not available on ${date}`);
+        }
+
+        this.screenings.push({
+            date,
+            hall,
+            description
+        });
+
+        return `New screening of ${this.movieName} is added.`;
+    }
+
+    endScreening(date, hall, soldTickets) {
+        let screening = this.screenings.find(s => s.date == date && s.hall == hall)
+        if (!screening) {
+            throw new Error(`Sorry, there is no such screening for ${this.movieName} movie.`);
+        }
+
+        let index = this.screenings.indexOf(screening);
+        this.screenings.splice(index, 1);
+
+        let currentProfit = this.ticketPrice * soldTickets;
+
+        this.totalProfit += currentProfit;
+        this.totalTicketsSold += soldTickets;
+        return `${this.movieName} movie screening on ${date} in ${hall} hall has ended. Screening profit: ${currentProfit}`;
+    }
+
+    toString() {
+        let result = [`${this.movieName} full information:`,
+        `Total profit: ${this.totalProfit.toFixed(0)}$`,
+        `Sold Tickets: ${this.totalTicketsSold}`];
+
+        if (this.screenings.length == 0) {
+            result.push('No more screenings!');
+        } else {
+            result.push('Remaining film screenings:');
+            this.screenings
+                .sort((a, b) => a.hall.localeCompare(b.hall))
+                .forEach(s => {
+                    result.push(`${s.hall} - ${s.date} - ${s.description}`);
+                });
+        }
+
+        return result.join('\n');
     }
 }
 
@@ -18,8 +65,8 @@ console.log(m.endScreening('October 2, 2020', 'IMAX 3D', 150));
 console.log(m.endScreening('October 3, 2020', 'Main', 78));
 console.log(m.toString());
 
-m.newScreening('October 4, 2020', '235', `regular`);
-m.newScreening('October 5, 2020', 'Main', `regular`);
-m.newScreening('October 3, 2020', '235', `regular`);
-m.newScreening('October 4, 2020', 'Main', `regular`);
+console.log(m.newScreening('October 4, 2020', '235', `regular`));
+console.log(m.newScreening('October 5, 2020', 'Main', `regular`));
+console.log(m.newScreening('October 3, 2020', '235', `regular`));
+console.log(m.newScreening('October 4, 2020', 'Main', `regular`));
 console.log(m.toString());
--- JS Advanced/Exams/27-06-21/02-restaurant.js
+++ JS Advanced/Exams/27-06-21/02-restaurant.js
@@ -1,6 +1,6 @@
 class Restaurant {
     constructor(budgetMoney) {
-        this.budgetMoney = budgetMoney;
+        this.budgetMoney = Number(budgetMoney);
         this.menu = {};
         this.stockProducts = {};
         this.history = [];
@@ -29,31 +29,69 @@ class Restaurant {
     addToMenu(meal, productsNeeded, price) {
 
         if (!this.menu[meal]) {
-            this.menu[meal] = { products: productsNeeded, price: price };
+            this.menu[meal] = {
+                products: {},
+                price: price
+            };
+
+            productsNeeded.forEach((el) => {
+                let [name, quantity] = el.split(' ');
+                quantity = Number(quantity);
+                this.menu[meal].products[name] = quantity;
+            })
+
             let mealsCount = Object.keys(this.menu).length;
             if (mealsCount == 1) {
-                console.log(`Great idea! Now with the ${meal} we have 1 meal in the menu, other ideas?`);
+                return `Great idea! Now with the ${meal} we have 1 meal in the menu, other ideas?`;
             } else {
-                console.log(`Great idea! Now with the ${meal} we have ${mealsCount} meals in the menu, other ideas?`);
+                return `Great idea! Now with the ${meal} we have ${mealsCount} meals in the menu, other ideas?`;
             }
         } else {
-            console.log(`The ${meal} is already in the our menu, try something different.`);
+            return `The ${meal} is already in the our menu, try something different.`;
         }
 
     }
 
     showTheMenu() {
-        return Object.entries(this.menu).forEach(([key, value]) => console.log(`${key} = $ ${value.price}`));
+        if (Object.keys(this.menu).length == 0) {
+            return "Our menu is not ready yet, please come later...";
+        } else {
+            let result = [];
+
+            for (const meal in this.menu) {
+                result.push(`${meal} - $ ${this.menu[meal].price}`);
+            }
+            return result.join('\n');
+            // return Object.entries(this.menu).forEach(([key, value]) => console.log(`${key} = $ ${value.price}`));
+        }
     }
 
-    makeTheOrder(orderedMeal) {
-        if (!this.menu[orderedMeal]) {
-            `There is not ${orderedMeal} yet in our menu, do you want to order something else?`;
+    makeTheOrder(meal) {
+        if (!this.menu[meal]) {
+            return `There is not ${meal} yet in our menu, do you want to order something else?`;
+        } else {
+            let productsNeeded = {};
+            for (const product in this.menu[meal].products) {
+                if (!this.stockProducts[product] || this.stockProducts[product] < this.menu[meal].products[product]) {
+                    return `For the time being, we cannot complete your order (${meal}), we are very sorry...`;
+                } else {
+                    productsNeeded[product] = this.menu[meal].products[product];
+                }
+            }
+
+            for (const prodNeeded in productsNeeded) {
+                this.stockProducts[prodNeeded] -= productsNeeded[prodNeeded];
+
+            }
         }
+        this.budgetMoney += this.menu[meal].price;
+        return `Your order (${meal}) will be completed in the next 30 minutes and will cost you ${this.menu[meal].price}.`
     }
 }
 
+
 let test = new Restaurant(1000);
+test.loadProducts(['Yogurt 30 3', 'Honey 50 4', 'Strawberries 20 10', 'Banana 5 1']);
 test.addToMenu('frozenYogurt', ['Yogurt 1', 'Honey 1', 'Banana 1', 'Strawberries 10'], 9.99);
-let res = test.addToMenu('Pizza', ['Flour 0.5', 'Oil 0.2', 'Yeast 0.5', 'Salt 0.1', 'Sugar 0.1', 'Tomato sauce 0.5', 'Pepperoni 1', 'Cheese 1.5'], 15.55);
+let res = test.makeTheOrder('frozenYogurt');
 
